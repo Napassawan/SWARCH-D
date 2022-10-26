147c3e3206d4f0c086ae967248b076102ac52df2
Napassawan <48213873+Napassawan@users.noreply.github.com>
2022-10-26 19:31:57 +0700

    Delete PatternDesign directory

--- PatternDesign/creational/JSONBookMetadataExporter.java
+++
@@ -1,8 +0,0 @@
-package creational;
-
-public class JSONBookMetadataExporter extends BookMetadataExporter {
-    @Override
-	public BookMetadataFormatter createFormatter() {
-		return new JSONBookMetadataFormatter();
-	}
-}
