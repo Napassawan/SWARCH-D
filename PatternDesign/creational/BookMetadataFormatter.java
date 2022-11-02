fabdaa6491b5dd7c262a6d289e418d618560cc07
Napassawan <48213873+Napassawan@users.noreply.github.com>
2022-11-02 20:41:00 +0700

    Add files via upload

---
+++ PatternDesign/creational/BookMetadataFormatter.java
@@ -0,0 +1,8 @@
+//ไม่แก้
+package creational;
+
+public interface BookMetadataFormatter {
+    BookMetadataFormatter reset();
+    BookMetadataFormatter append(Book b);
+    String getMetadataString();
+}
